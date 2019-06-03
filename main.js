c0a74cebae3d0b6b6c5d2c6b584473bc2ea4f6a2
bill <dz218218@gmail.com>
2019-06-04 00:15:45 +0800

    its ok

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-setTimeout(function(){
+setTimeout(function() {
 	$('.images>img:nth-child(1)').css({
 		transform: 'translate(-100%)'
 	})
 	$('.images>img:nth-child(2)').css({
 		transform: 'translate(-100%)'
 	})
-},3000)
 
-setTimeout(function(){
+}, 3000)
+
+setTimeout(function() {
 	$('.images>img:nth-child(2)').css({
 		transform: 'translate(-200%)'
 	})
 	$('.images>img:nth-child(3)').css({
 		transform: 'translate(-100%)'
 	})
-},6000)
+
+}, 6000)
+
+
+
