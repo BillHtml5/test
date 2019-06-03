313077d71e0a24df9940500cff1d8b71477c9321
bill <dz218218@gmail.com>
2019-06-03 23:51:01 +0800

    its ok

---
+++ main.js
@@ -0,0 +1,17 @@
+setTimeout(function(){
+	$('.images>img:nth-child(1)').css({
+		transform: 'translate(-100%)'
+	})
+	$('.images>img:nth-child(2)').css({
+		transform: 'translate(-100%)'
+	})
+},3000)
+
+setTimeout(function(){
+	$('.images>img:nth-child(2)').css({
+		transform: 'translate(-200%)'
+	})
+	$('.images>img:nth-child(3)').css({
+		transform: 'translate(-100%)'
+	})
+},6000)
