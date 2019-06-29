674e4ea514a7e85607b22807cae7af950622be39
bill <dz218218@gmail.com>
2019-06-29 11:08:44 +0800

    reactdemo创建组件

--- React/reactdemo01/src/App.js
+++ React/reactdemo01/src/App.js
@@ -3,12 +3,15 @@ import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 
 import Home from './components/Home'
+import News from './components/News'
 
 function App() {
   return (
     <div className="App">
      React
 		 <Home></Home>
+		 
+		 <News></News>
     </div>
   );
 }
---
+++ React/reactdemo01/src/components/News.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class News extends React.Component{
+	
+	constructor(props){
+		super(props);// 用于父子组件传值
+	}
+	
+	render(){
+		
+		return(
+		<div>这是个消息</div>
+		)
+	}
+}
+export default News;
