cabbf4e094d2d99ca2b9fd71738e0458ba29c98d
bill <dz218218@gmail.com>
2019-07-04 13:50:48 +0800

    图片引入

--- React/reactdefault/src/App.js
+++ React/reactdefault/src/App.js
@@ -2,12 +2,15 @@ import React,{Conponent}from 'react';
 import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 import Home from './components/Home';
+import News from './components/News';
 
 function App() {
   return (
     <div className="App">
      react根目录
 		 <Home></Home>
+		 
+		 <News></News>
     </div>
   );
 }
--- React/reactdefault/src/components/News.js
+++ React/reactdefault/src/components/News.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import photo from '../assets/images/bill.jpg';
 
 class News extends React.Component{
 	
+	constructor(props){
+		super(props);
+		
+	this.state={
+		msg:'bill'
+	}
+	}
 	
+	render(){
+		return(
+		<div>
+			<img src={photo}/>
+		</div>
+		
+		)
+	}
 }
 
 export default News;
