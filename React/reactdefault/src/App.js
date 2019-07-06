b2d21b8e0d7e3368a166bb0f2818a08f8d47d84c
bill <dz218218@gmail.com>
2019-07-06 11:07:27 +0800

    Event数据绑定，改变this指向

--- React/reactdefault/src/App.js
+++ React/reactdefault/src/App.js
@@ -3,11 +3,12 @@ import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 import Home from './components/Home';
 import News from './components/News';
-
+import Event from './components/Event';
 function App() {
   return (
     <div className="App">
      react根目录
+		<Event ></Event >
 		 <Home></Home>
 		 
 		 <News></News>
--- React/reactdefault/src/components/Event.js
+++ React/reactdefault/src/components/Event.js
@@ -1,21 +1,53 @@
 import React from 'react';
 
 
-class Event extends React.Component{
+class Event extends React.Component {
 	
-	constructor(props){
+	constructor(props) {
+		super(props);
 		this.state={
-			msg:'sssss'
+			msg: 'run方法',
+			msg1:'数据绑定',
 		}
+		//第2种改变this指向的方法
+		this.getMsg2 =this.getMsg2.bind(this);
 	}
-	
-	render(){
-		<div>
-			<button></button>
+	run(){
+		alert('这是一个run方法')
 		
-		</div>
 	}
 	
+	getMsg1(){
+		alert(this.state.msg1)
+		}
+		
+	getMsg2(){
+		alert(this.state.msg1)
+		}
+		
+	getMsg3=()=>{
+		alert(this.state.msg1)
+		}
+		
+	
+	
+	render() {
+		return ( 
+		<div>
+			<button onClick = {this.run} >run方法< /button>
+			
+			<hr/>
+			<button onClick = {this.getMsg1.bind(this)} >获取数据--第一种改变this指向的方法< /button>
+
+			<hr/>
+			<button onClick = {this.getMsg2} >获取数据--第2种改变this指向的方法< /button>
+			<hr/>
+			<button onClick = {this.getMsg3} >获取数据--第3种改变this指向的方法< /button>
+			</div>
+		)
+
+	}
+
 }
 
 export default Event;
