94de24ba8ed19e1f2bdb63c1f0d29d78cb892114
bill <dz218218@gmail.com>
2019-07-06 10:12:56 +0800

    Event

---
+++ React/reactdefault/src/components/Event.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Event extends React.Component{
+	
+	constructor(props){
+		this.state={
+			msg:'sssss'
+		}
+	}
+	
+	render(){
+		<div>
+			<button></button>
+		
+		</div>
+	}
+	
+}
+
+export default Event;
