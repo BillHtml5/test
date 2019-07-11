53885f4f85c76b3c668cef293df3249736c01ff6
bill <dz218218@gmail.com>
2019-07-11 22:43:55 +0800

    react list

--- React/reactdefault/src/components/List.js
+++ React/reactdefault/src/components/List.js
@@ -3,13 +3,22 @@ import React from 'react';
 class List extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+			username:''
+		};
     }
 
     render() {
         return (
             <div>
-                List组件
+                 {/* 获取表单的值
+
+                1、监听表单的改变事件                        onChange
+                2、在改变的事件里面获取表单输入的值            ref获取 
+                3、把表单输入的值赋值给username              this.setState({})
+                4、点击按钮的时候获取 state里面的username     this.state.username
+                 */}
+				 
             </div>
         );
     }
